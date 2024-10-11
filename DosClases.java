4da0aaa76ea78008f777d8e8a6ca3912b6099c6c
juanCaro16 <168235896+juanCaro16@users.noreply.github.com>
2024-10-11 14:43:05 -0500

    Add files via upload

---
+++ DosClases.java
@@ -0,0 +1,13 @@
+package laboratorio12;
+
+public class DosClases {
+
+	public static void main(String agrs[]) {
+		
+		Entidad rectangulo;
+		rectangulo = new Entidad();
+		
+		rectangulo.ingresarDatos();
+		rectangulo.calcularArea();
+	}
+}
